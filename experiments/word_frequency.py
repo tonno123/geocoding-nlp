ca8794ff833df7b4bc055b6d9497f895e29cd1bc
tonno123 <toonmeijer@gmail.com>
2019-11-27 19:56:52 +0100

    improved word_frequency.py to filter data better, added comments to Data Visualisations

--- experiments/word_frequency.py
+++ experiments/word_frequency.py
@@ -8,9 +8,6 @@ from import_data import import_data
 from collections import Counter
 from search_pattern import search_pattern
 
-nlp = spacy.load('nl_core_news_sm')
-data = import_data('hetongelukscraped.csv', column="Artikel")
-data = data[:50]
 
 def word_count():
     words = []
@@ -42,7 +39,7 @@ def word_count():
 # in the dataset. It outputs a graph showing the most frequently occurring combinations
 # of words of POS-tags together with the location mentions. It also outputs a dict
 # which contains the locations of all relevant span combinations in the dataset.
-def word_pos_frequency(input_data, word_list=[], pos_list=[]):
+def word_pos_frequency(input_data, word_list=[], pos_list=[], pos_or_word=1):
     words = []
     span_counter = 0
     span_location = {}
@@ -58,35 +55,41 @@ def word_pos_frequency(input_data, word_list=[], pos_list=[]):
                 current_span = input_data[article_n][sentence_n][span_n]
                 span_str = current_span.text.strip('.')
                 for entity in current_span._.entities:
-                    span_str = span_str.replace(entity.text, "#LOC", 1)
+                    span_str = span_str.replace(entity.text, "#loc", 1)
 
 
                 # in case one or more words are entered
                 if word_list:
-                    for word in span_str.split():
-                        if word.lower() in word_list or word == "#LOC":
-                            new_span = new_span + word.lower() + ' '
+                    if any(elem in span_str.split() for elem in word_list) and "#loc" in span_str.split():
+                        for word in span_str.split():
+                            if word.lower() in word_list or word == "#loc":
+                                new_span = new_span + word.lower() + ' '
 
                 # in case one or more POS-tags are entered
                 elif pos_list:
                     for i, word in enumerate(span_str.split()):
                         if current_span[i].pos_ in pos_list:
-                            new_span = new_span + word.lower() + ' '
-                        elif word == "#LOC":
+                            if pos_or_word:
+                                new_span = new_span + current_span[i].pos_ + ' '
+                            else:
+                                new_span = new_span + word.lower() + ' '
+                        elif word == "#loc":
                             new_span = new_span + "#loc" + ' '
-
+                    if all(i == "#loc" for i in new_span.split()) or not "#loc" in new_span:
+                        new_span = ''
                 new_span = new_span[:-1]
 
                 # this saves all word and POS-tag combinations in a dict, where
                 # the key is the combination itself, and the key is a list of
                 # numbers that indicate where in the data the combination occurs.
-                if new_span in span_location:
-                    span_location[new_span].append(span_counter)
-                else:
-                    span_location[new_span] = [span_counter]
+                if new_span != '':
+                    if new_span in span_location:
+                        span_location[new_span].append(span_counter)
+                    else:
+                        span_location[new_span] = [span_counter]
+                    words.append(new_span)
                 span_counter = span_counter + 1
 
-                words.append(new_span)
 
     ent_freq = Counter(words)
     keys, values = zip(*ent_freq.most_common(50))
@@ -98,3 +101,14 @@ def word_pos_frequency(input_data, word_list=[], pos_list=[]):
     plt.xticks(rotation=85)
     plt.show()
     return span_location
+
+
+# nlp = spacy.load('nl_core_news_sm')
+# data = import_data('hetongelukscraped.csv', column="Artikel")
+# data = data[:150]
+#
+# input_data = get_location_descriptions(data, nlp)
+#
+# word_pos_location_dict4 = word_pos_frequency(input_data, pos_list=["ADP"], pos_or_word=0)
+#
+# search_pattern("ADP #loc", word_pos_location_dict4, input_data)
